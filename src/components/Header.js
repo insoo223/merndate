166a8f5e87b64687356cb08b479139f243875a7e
insoo223 <insoo@hotmail.com>
2024-10-07 13:38:50 +0900

    creating a header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,18 @@
 import React from 'react'
-//import './Header.css'
-import PersonIcon from '@mui/icons-material/Person';
+import './Header.css'
 import IconButton from '@mui/material/IconButton';
+import PersonIcon from '@mui/icons-material/Person';
+import ForumIcon from '@mui/icons-material/Forum';
 
 const Header = () => {
 	return (
 		<div className='header'>
 			<IconButton>
 				<PersonIcon fontSize='large' className='header_icon'/>	
+			</IconButton>
+			<img className='header_logo' src='logo192.png' alt='header'/>	
+			<IconButton>
+				<ForumIcon fontSize='large' className='header_icon'/>	
 			</IconButton>
 		</div>
 	) //return
