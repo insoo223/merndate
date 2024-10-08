19c89bae23a75f03e02d9373ca5a47c93fe5acf4
insoo223 <insoo@hotmail.com>
2024-10-08 12:16:22 +0900

    init success on swipe button compo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './cmp/Header';
 import DatingCards from './cmp/DatingCards';
+import SwipeButtons from './cmp/SwipeButtons';
 
 function App() {
   return (
     <div className="App">
 			<Header/>	
 			<DatingCards/>
+			<SwipeButtons/>
     </div>
   );
 }
