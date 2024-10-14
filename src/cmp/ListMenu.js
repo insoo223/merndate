95bca70d6351cd60ee43b60367307b1d5f791ffa
insoo223 <insoo@hotmail.com>
2024-10-14 20:59:20 +0900

    init React Router

--- src/cmp/ListMenu.js
+++ src/cmp/ListMenu.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom'
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
@@ -8,21 +9,20 @@ import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
 
 export default function ListMenu({menu}) {
- 
   return (
-      <List>
-        {menu.map((text, index) => (
+    <List>
+      {menu.map((text, index) => (
+        <Link to={"/" + text}>
           <ListItem key={text} disablePadding>
-            <ListItemButton
-                onClick={(event) => {
-                    console.log(text + " clicked");}}>
+            <ListItemButton onClick={() => {console.log(text + " clicked")}}>
               <ListItemIcon>
                 {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
               </ListItemIcon>
               <ListItemText primary={text} />
             </ListItemButton>
           </ListItem>
-        ))}
-      </List>
+        </Link>
+      ))}
+    </List>
   ); // return
 } // ListMenu
--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import BasicMenu from './cmp/BasicMenu';
+import FormikCard from './cmp/FormikCard';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "Admin1/",
+    element: <FormikCard />,
+  },  
+  {
+    path: "Admin2/",
+    element: <BasicMenu />,
+  },    
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
